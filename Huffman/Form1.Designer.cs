4cef71dd0569cadb9527079ecb80dbdb5869a876
Данияр р <danisoftbk.ru@outlook.com>
2025-07-15 21:33:42 +0500

    Добавьте файлы проекта.

---
+++ Huffman/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace Huffman
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEncode = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEncoded = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dataGridViewFrequencies = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridViewCodes = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.treePanel = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAscii = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFrequencies)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCodes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtInput.Location = new System.Drawing.Point(12, 28);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(927, 100);
+            this.txtInput.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Исходный текст";
+            // 
+            // btnEncode
+            // 
+            this.btnEncode.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEncode.Location = new System.Drawing.Point(865, 160);
+            this.btnEncode.Name = "btnEncode";
+            this.btnEncode.Size = new System.Drawing.Size(75, 23);
+            this.btnEncode.TabIndex = 2;
+            this.btnEncode.Text = "Кодировать";
+            this.btnEncode.UseVisualStyleBackColor = true;
+            this.btnEncode.Click += new System.EventHandler(this.btnEncode_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Закодированный текст";
+            // 
+            // txtEncoded
+            // 
+            this.txtEncoded.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtEncoded.Location = new System.Drawing.Point(12, 202);
+            this.txtEncoded.Multiline = true;
+            this.txtEncoded.Name = "txtEncoded";
+            this.txtEncoded.ReadOnly = true;
+            this.txtEncoded.Size = new System.Drawing.Size(927, 60);
+            this.txtEncoded.TabIndex = 3;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 268);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(300, 349);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridViewFrequencies);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage1.Size = new System.Drawing.Size(292, 323);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Таблица частот";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewFrequencies
+            // 
+            this.dataGridViewFrequencies.AllowUserToAddRows = false;
+            this.dataGridViewFrequencies.AllowUserToDeleteRows = false;
+            this.dataGridViewFrequencies.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewFrequencies.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridViewFrequencies.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewFrequencies.Location = new System.Drawing.Point(3, 3);
+            this.dataGridViewFrequencies.Name = "dataGridViewFrequencies";
+            this.dataGridViewFrequencies.ReadOnly = true;
+            this.dataGridViewFrequencies.RowHeadersWidth = 51;
+            this.dataGridViewFrequencies.Size = new System.Drawing.Size(286, 317);
+            this.dataGridViewFrequencies.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Символ";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Частота";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 125;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridViewCodes);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage2.Size = new System.Drawing.Size(292, 323);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Таблица кодов";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewCodes
+            // 
+            this.dataGridViewCodes.AllowUserToAddRows = false;
+            this.dataGridViewCodes.AllowUserToDeleteRows = false;
+            this.dataGridViewCodes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCodes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2});
+            this.dataGridViewCodes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewCodes.Location = new System.Drawing.Point(3, 3);
+            this.dataGridViewCodes.Name = "dataGridViewCodes";
+            this.dataGridViewCodes.ReadOnly = true;
+            this.dataGridViewCodes.RowHeadersWidth = 51;
+            this.dataGridViewCodes.Size = new System.Drawing.Size(286, 317);
+            this.dataGridViewCodes.TabIndex = 1;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Символ";
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            this.dataGridViewTextBoxColumn1.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "Код";
+            this.dataGridViewTextBoxColumn2.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            this.dataGridViewTextBoxColumn2.Width = 125;
+            // 
+            // treePanel
+            // 
+            this.treePanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treePanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.treePanel.Location = new System.Drawing.Point(318, 268);
+            this.treePanel.Name = "treePanel";
+            this.treePanel.Size = new System.Drawing.Size(622, 349);
+            this.treePanel.TabIndex = 6;
+            this.treePanel.Paint += new System.Windows.Forms.PaintEventHandler(this.treePanel_Paint_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(315, 252);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Дерево Хаффмана";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 252);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Таблицы";
+            // 
+            // txtAscii
+            // 
+            this.txtAscii.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtAscii.Location = new System.Drawing.Point(12, 134);
+            this.txtAscii.Multiline = true;
+            this.txtAscii.Name = "txtAscii";
+            this.txtAscii.ReadOnly = true;
+            this.txtAscii.Size = new System.Drawing.Size(927, 20);
+            this.txtAscii.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 118);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "ASCII бинарный вид";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(952, 629);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtAscii);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.treePanel);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEncoded);
+            this.Controls.Add(this.btnEncode);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInput);
+            this.MinimumSize = new System.Drawing.Size(600, 398);
+            this.Name = "Form1";
+            this.Text = "Кодирование Хаффмана";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFrequencies)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCodes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEncode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEncoded;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView dataGridViewFrequencies;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridViewCodes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.Panel treePanel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAscii;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ Huffman/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Huffman
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            treePanel.Paint += TreePanel_Paint;
+        }
+
+        private HuffmanTree huffmanTree;
+        private Dictionary<char, string> huffmanCodes;
+        private Dictionary<char, int> frequencies;
+
+        private void btnEncode_Click(object sender, EventArgs e)
+        {
+            string inputText = txtInput.Text;
+            if (string.IsNullOrEmpty(inputText))
+            {
+                MessageBox.Show("Введите текст для кодирования");
+                return;
+            }
+
+            // Строим дерево Хаффмана
+            huffmanTree = new HuffmanTree();
+            huffmanTree.Build(inputText);
+            frequencies = huffmanTree.GetFrequencies();
+            huffmanCodes = huffmanTree.GetCodes();
+
+            // Обновляем таблицы
+            UpdateFrequencyTable();
+            UpdateCodeTable();
+
+            // Показываем ASCII представление (группы по 4 бита)
+            txtAscii.Text = GetAsciiBinaryString(inputText);
+
+            // Кодируем текст по Хаффману
+            string encodedText = huffmanTree.Encode(inputText);
+            txtEncoded.Text = encodedText;
+
+            // Перерисовываем дерево
+            treePanel.Invalidate();
+        }
+
+        private string GetAsciiBinaryString(string text)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in text)
+            {
+                string binary = Convert.ToString(c, 2).PadLeft(8, '0');
+                for (int i = 0; i < binary.Length; i += 4)
+                {
+                    int length = Math.Min(4, binary.Length - i);
+                    sb.Append(binary.Substring(i, length) + " ");
+                }
+            }
+            return sb.ToString().Trim();
+        }
+
+        private void UpdateFrequencyTable()
+        {
+            dataGridViewFrequencies.Rows.Clear();
+            foreach (var pair in frequencies.OrderByDescending(p => p.Value))
+            {
+                dataGridViewFrequencies.Rows.Add(pair.Key, pair.Value);
+            }
+        }
+
+        private void UpdateCodeTable()
+        {
+            dataGridViewCodes.Rows.Clear();
+            foreach (var pair in huffmanCodes.OrderBy(p => p.Value.Length))
+            {
+                dataGridViewCodes.Rows.Add(pair.Key, pair.Value);
+            }
+        }
+
+        private void TreePanel_Paint(object sender, PaintEventArgs e)
+        {
+            if (huffmanTree == null) return;
+
+            Graphics g = e.Graphics;
+            g.Clear(Color.White);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            int startX = treePanel.Width / 2;
+            int startY = 30;
+            int nodeRadius = 20;
+            int verticalSpacing = 60;
+
+            DrawTree(g, huffmanTree.Root, startX, startY, nodeRadius, verticalSpacing, treePanel.Width / 4);
+        }
+
+        private void DrawTree(Graphics g, HuffmanNode node, int x, int y, int radius, int verticalSpacing, int horizontalSpacing)
+        {
+            if (node == null) return;
+
+            // Рисуем узел
+            g.FillEllipse(Brushes.LightBlue, x - radius, y - radius, radius * 2, radius * 2);
+            g.DrawEllipse(Pens.Black, x - radius, y - radius, radius * 2, radius * 2);
+
+            string nodeText = node.Symbol == '\0' ? node.Frequency.ToString() : $"{node.Symbol}:{node.Frequency}";
+            SizeF textSize = g.MeasureString(nodeText, Font);
+            g.DrawString(nodeText, Font, Brushes.Black, x - textSize.Width / 2, y - textSize.Height / 2);
+
+            // Рисуем связи с детьми
+            if (node.Left != null)
+            {
+                int childX = x - horizontalSpacing;
+                int childY = y + verticalSpacing;
+                g.DrawLine(Pens.Black, x, y + radius, childX, childY - radius);
+                g.DrawString("0", Font, Brushes.Red, (x + childX) / 2, (y + childY) / 2);
+                DrawTree(g, node.Left, childX, childY, radius, verticalSpacing, horizontalSpacing / 2);
+            }
+
+            if (node.Right != null)
+            {
+                int childX = x + horizontalSpacing;
+                int childY = y + verticalSpacing;
+                g.DrawLine(Pens.Black, x, y + radius, childX, childY - radius);
+                g.DrawString("1", Font, Brushes.Red, (x + childX) / 2, (y + childY) / 2);
+                DrawTree(g, node.Right, childX, childY, radius, verticalSpacing, horizontalSpacing / 2);
+            }
+        }
+
+        private void treePanel_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+
+    public class HuffmanNode : IComparable<HuffmanNode>
+    {
+        public char Symbol { get; set; }
+        public int Frequency { get; set; }
+        public HuffmanNode Left { get; set; }
+        public HuffmanNode Right { get; set; }
+
+        public int CompareTo(HuffmanNode other)
+        {
+            return Frequency.CompareTo(other.Frequency);
+        }
+
+        public bool IsLeaf()
+        {
+            return Left == null && Right == null;
+        }
+    }
+
+    public class HuffmanTree
+    {
+        public HuffmanNode Root { get; private set; }
+        private Dictionary<char, int> frequencies;
+
+        public void Build(string source)
+        {
+            frequencies = new Dictionary<char, int>();
+            foreach (char c in source)
+            {
+                if (!frequencies.ContainsKey(c))
+                    frequencies[c] = 0;
+                frequencies[c]++;
+            }
+
+            var priorityQueue = new List<HuffmanNode>();
+            foreach (var symbol in frequencies)
+            {
+                priorityQueue.Add(new HuffmanNode { Symbol = symbol.Key, Frequency = symbol.Value });
+            }
+
+            while (priorityQueue.Count > 1)
+            {
+                priorityQueue.Sort();
+
+                var left = priorityQueue[0];
+                var right = priorityQueue[1];
+
+                var parent = new HuffmanNode()
+                {
+                    Symbol = '\0',
+                    Frequency = left.Frequency + right.Frequency,
+                    Left = left,
+                    Right = right
+                };
+
+                priorityQueue.Remove(left);
+                priorityQueue.Remove(right);
+                priorityQueue.Add(parent);
+            }
+
+            Root = priorityQueue.FirstOrDefault();
+        }
+
+        public Dictionary<char, string> GetCodes()
+        {
+            var codes = new Dictionary<char, string>();
+            Traverse(Root, "", codes);
+            return codes;
+        }
+
+        private void Traverse(HuffmanNode node, string code, Dictionary<char, string> codes)
+        {
+            if (node == null) return;
+
+            if (node.IsLeaf())
+            {
+                if (code == "")
+                    code = "0"; // для случая, когда всего один символ
+                codes[node.Symbol] = code;
+                return;
+            }
+
+            Traverse(node.Left, code + "0", codes);
+            Traverse(node.Right, code + "1", codes);
+        }
+
+        public Dictionary<char, int> GetFrequencies()
+        {
+            return frequencies;
+        }
+
+        public string Encode(string source)
+        {
+            var codes = GetCodes();
+            string encoded = "";
+            foreach (char c in source)
+            {
+                encoded += codes[c];
+            }
+            return encoded;
+        }
+    }
+}
